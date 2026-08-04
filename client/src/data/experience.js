302c01fdb4ffabf54887f420a2e1ff78cf7af6a9
Amit Kumar <amitkumarmca7393@gmail.com>
2026-08-05 04:13:14 +0530

    Add experience data for internships and certifications

---
+++ client/src/data/experience.js
@@ -0,0 +1,42 @@
+const experience = [
+  {
+    id: 1,
+    role: "Web Development Intern",
+    company: "CODSOFT",
+    type: "Virtual Internship",
+    duration: "01 July 2026 – 31 July 2026",
+    description:
+      "Successfully completed a 4-week virtual internship program in Web Development with practical experience in building responsive web applications and completing development tasks.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React.js",
+      "Git",
+      "GitHub",
+      "Responsive Web Design",
+    ],
+    certificate: "CODSOFT Web Development Internship Certificate",
+  },
+  {
+    id: 2,
+    role: "React & Redux Certification",
+    company: "KnowledgeGate",
+    type: "Certification",
+    duration: "Issued July 5, 2026",
+    description:
+      "Successfully completed React and Redux certification with a score of 90%, demonstrating knowledge of React development and state management using Redux.",
+    skills: [
+      "React.js",
+      "Redux",
+      "Redux Toolkit",
+      "JavaScript",
+      "State Management",
+    ],
+    certificateId: "CERT-1783268475042-2E0765E0",
+    verificationUrl:
+      "https://knowledgegate.in/certificate/842C7A86",
+  },
+];
+
+export default experience;
